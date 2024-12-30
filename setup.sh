97e3e60c1ead13c14146407caf347c58a16362ce
SJ WONG <sjwong@SJs-MacBook-Air.local>
2024-12-30 16:50:25 +0800

    initial commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# conda create -n density_env python=3.9
+ conda activate density_env
+ conda install -c conda-forge nibabel numpy scipy matplotlib mayavi notebook -y
+
+#  for mac user or facing issues with mayavi
+# conda install -c conda-forge pyqt wxpython vtk -y
+
+# for VS Code
+# make sure CMD + SHIFT + P to refresh window first
+# make sure CMD + SHIFT + P to choose the Python: Intepreter
